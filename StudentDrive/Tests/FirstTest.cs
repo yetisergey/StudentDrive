735248f5b4417329ee19707b013d98c170817c95
Сергей Масленников <yetisergey@gmail.com>
2018-04-27 11:36:48 +0300

    fix csproj

---
+++ StudentDrive/Tests/FirstTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentDrive.Controllers;
+using System.Web.Mvc;
+
+namespace Tests
+{
+    [TestClass]
+    public class FirstTest
+    {
+        [TestMethod]
+        public void IsNotNullMethod()
+        {
+            //HomeController hc = new HomeController();
+            //var result = hc.File() as ViewResult;
+           // Assert.IsNotNull(result);
+        }
+    }
+}
